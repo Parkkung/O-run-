1eb398fe539a75c530ca065fa3b9c3f14d15b6c1
Parkkung <parksuke47@gmail.com>
2019-04-05 15:27:32 +0700

    background

--- worlds.py
+++ worlds.py
@@ -1,5 +1,9 @@
 import arcade
 from character import Ora, World
+# import codecs
+# with codecs.open('worlds.py', 'r', encoding='utf-8',
+#                  errors='ignore') as fdata:
+
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -11,22 +15,28 @@ class OrunGameWindow(arcade.Window):
         super().__init__(width, height)
 
         self.world = World(width,height)
+        self.background = None
+        # arcade.set_background_color(arcade.color.BABY_BLUE)
 
-        arcade.set_background_color(arcade.color.BABY_BLUE)
+    def setup(self):
+        self.background = arcade.load_texture("images/Background.png")
 
     def on_draw(self):
         arcade.start_render()
+        arcade.draw_texture_rectangle(SCREEN_WIDTH//2, SCREEN_HEIGHT//2,
+                                      SCREEN_WIDTH,SCREEN_HEIGHT, self.background)
+
         arcade.draw_text(str(self.world.score),
                          self.width - 30, self.height - 30,
-                         arcade.color.YELLOW_ORANGE, 20)
+                         arcade.color.GREEN, 20)
 
     def update(self, delta):
         self.world.update(delta)
 
 def main():
-    OrunGameWindow(SCREEN_WIDTH,SCREEN_HEIGHT)
-    arcade.run
+    window = OrunGameWindow(SCREEN_WIDTH,SCREEN_HEIGHT)
+    window.setup()
+    arcade.run()
 
 if __name__ == '__main__':
-    window = OrunGameWindow(SCREEN_WIDTH,SCREEN_HEIGHT)
-    arcade.run()
+    main()
